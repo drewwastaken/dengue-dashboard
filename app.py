d3723bcbfc0d1d4639fa601d1fd0b775d3bd9159
Nicholas Andrew Alcantara <nicholasandrewalcantara0@gmail.com>
2025-12-18 21:28:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import plotly.graph_objects as go
 from datetime import datetime
 
 # Page configuration
@@ -347,4 +346,5 @@ st.markdown("""
     </div>
 
 """, unsafe_allow_html=True)
+
 
